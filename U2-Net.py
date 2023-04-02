091a22bdd402475eab71883ee478d783525df158
MarkovChen <98240703+MakovChen@users.noreply.github.com>
2023-04-03 03:42:59 +0800

    Update U2-Net.py

--- U2-Net.py
+++ U2-Net.py
@@ -78,107 +78,71 @@ def RSU5(x, mid_ch=12, out_ch=3):
     x0 = REBNCONV(x, out_ch, 1)
     x1 = REBNCONV(x0, mid_ch, 1)
     x = MaxPool2D(2, 2)(x1)
-
     x2 = REBNCONV(x, mid_ch, 1)
     x = MaxPool2D(2, 2)(x2)
-
     x3 = REBNCONV(x, mid_ch, 1)
     x = MaxPool2D(2, 2)(x3)
-
     x4 = REBNCONV(x, mid_ch, 1)
-
     x = REBNCONV(x, mid_ch, 2)
-
     x = REBNCONV(tf.concat([x,x4],axis=-1), mid_ch, 1)
     x = _upsample_like(x,x3)
-
     x = REBNCONV(tf.concat([x,x3],axis=-1), mid_ch, 1)
     x = _upsample_like(x,x2)
-
     x = REBNCONV(tf.concat([x,x2],axis=-1), mid_ch, 1)
     x = _upsample_like(x,x1)
-
     x = REBNCONV(tf.concat([x,x1],axis=-1), out_ch, 1)
-
     return x + x0
 
 def RSU4(x, mid_ch=12, out_ch=3):
-    
     x0 = REBNCONV(x, out_ch, 1)
-    
     x1 = REBNCONV(x0, mid_ch, 1)
     x = MaxPool2D(2, 2)(x1)
-
     x2 = REBNCONV(x, mid_ch, 1)
     x = MaxPool2D(2, 2)(x2)
-
     x3 = REBNCONV(x, mid_ch, 1)
-
     x = REBNCONV(x, mid_ch, 2)
-
     x = REBNCONV(tf.concat([x,x3],axis=-1), mid_ch, 1)
     x = _upsample_like(x,x2)
-
     x = REBNCONV(tf.concat([x,x2],axis=-1), mid_ch, 1)
     x = _upsample_like(x,x1)
-
     x = REBNCONV(tf.concat([x,x1],axis=-1), out_ch, 1)
-
     return x + x0
 
 def RSU4F(x, mid_ch=12, out_ch=3):
-
     x0 = REBNCONV(x, out_ch, 1)
-    
     x1 = REBNCONV(x0, mid_ch, 1)
     x2 = REBNCONV(x1, mid_ch, 2)
     x3 = REBNCONV(x2, mid_ch, 4)
-    
     x4 = REBNCONV(x3, mid_ch, 8)
-    
     x = REBNCONV(tf.concat([x4,x3],axis=-1), mid_ch, 4)
     x = REBNCONV(tf.concat([x,x2],axis=-1), mid_ch, 2)
     x = REBNCONV(tf.concat([x,x1],axis=-1), out_ch, 1)
-
     return x + x0
 
 def U2NET(x, out_ch=1):
-    
     x1 = RSU7(x, 32, 64)
     x = MaxPool2D(2, 2)(x1)
-
     x2 = RSU6(x, 32, 128)
     x = MaxPool2D(2, 2)(x2)
-
     x3 = RSU5(x, 64, 256)
     x = MaxPool2D(2, 2)(x3)
-
     x4 = RSU4(x, 128, 512)
     x = MaxPool2D(2, 2)(x4)
-
     x5 = RSU4F(x, 256, 512)
     x = MaxPool2D(2, 2)(x5)
-
     x6 = RSU4F(x, 256, 512)
     x = _upsample_like(x6,x5)
 
-    #-----------------decoder--------------------#
-
     x5 = RSU4F(tf.concat([x,x5],axis=-1),256, 512)
     x = _upsample_like(x5,x4)
-
     x4 = RSU4(tf.concat([x,x4],axis=-1),128, 256)
     x = _upsample_like(x4,x3)
-
     x3 = RSU5(tf.concat([x,x3],axis=-1),64, 128)
     x = _upsample_like(x3,x2)
-
     x2 = RSU6(tf.concat([x,x2],axis=-1),32, 64)
     x = _upsample_like(x2,x1)
-    
     x1 = RSU7(tf.concat([x,x1],axis=-1),16, 64)
 
-    #Side output
     x = ZeroPadding2D((1,1))(x1) 
     d1 = Conv2D(out_ch, 3)(x)
     d1 = Activation('sigmoid')(d1)
@@ -214,94 +178,66 @@ def U2NET(x, out_ch=1):
     return tf.stack([d0,d1,d2,d3,d4,d5,d6])
 
 def U2NETP(x, out_ch=1):
-    
     x1 = RSU7(x, 16, 64)
     x = MaxPool2D(2, 2)(x1)
-
     x2 = RSU6(x, 16, 64)
     x = MaxPool2D(2, 2)(x2)
-
     x3 = RSU5(x, 16, 64)
     x = MaxPool2D(2, 2)(x3)
-
     x4 = RSU4(x, 16, 64)
     x = MaxPool2D(2, 2)(x4)
-
     x5 = RSU4(x, 16, 64)
     x = MaxPool2D(2, 2)(x5)
-
     x6 = RSU4F(x, 16, 64)
     x = _upsample_like(x6,x5)
 
-    #---------------decoder--------------------
     x5 = RSU4F(tf.concat([x,x5],axis=-1),16, 64)
     x = _upsample_like(x5,x4)
-
     x4 = RSU4(tf.concat([x,x4],axis=-1),16, 64)
     x = _upsample_like(x4,x3)
-
     x3 = RSU5(tf.concat([x,x3],axis=-1),16, 64)
     x = _upsample_like(x3,x2)
-
     x2 = RSU6(tf.concat([x,x2],axis=-1),16, 64)
     x = _upsample_like(x2,x1)
-    
     x1 = RSU7(tf.concat([x,x1],axis=-1),16, 64)
-
     x = ZeroPadding2D((1,1))(x1) 
     d1 = Conv2D(out_ch, 3)(x)
     d1 = Activation('sigmoid')(d1)
-
     x = ZeroPadding2D((1,1))(x2) 
     x = Conv2D(out_ch, 3)(x)
     d2 = _upsample_like(x,d1)
     d2 = Activation('sigmoid')(d2)
-    
     x = ZeroPadding2D((1,1))(x3) 
     x = Conv2D(out_ch, 3)(x)
     d3 = _upsample_like(x,d1)
     d3 = Activation('sigmoid')(d3)
-    
     x = ZeroPadding2D((1,1))(x4) 
     x = Conv2D(out_ch, 3)(x)
     d4 = _upsample_like(x,d1)
     d4 = Activation('sigmoid')(d4)
-    
     x = ZeroPadding2D((1,1))(x5) 
     x = Conv2D(out_ch, 3)(x)
     d5 = _upsample_like(x,d1)
     d5 = Activation('sigmoid')(d5)
-    
     x = ZeroPadding2D((1,1))(x6) 
     x = Conv2D(out_ch, 3)(x)
     d6 = _upsample_like(x,d1)
     d6 = Activation('sigmoid')(d6)
-
     d0 = Conv2D(out_ch, 1)(tf.concat([d1,d2,d3,d4,d5,d6],axis=-1))
     d0 = Activation('sigmoid')(d0)
-
     return tf.stack([d0,d1,d2,d3,d4,d5,d6]) 
 
 net_input = Input(shape=(256,256,3)) 
-
 model_output = U2NET(net_input)
-
 model = Model(inputs = net_input, outputs = model_output)
-
 opt = tf.keras.optimizers.Adam(lr = 1e-3)
-
 bce = BinaryCrossentropy()
-
 model.compile(optimizer = opt, loss = loss, metrics = None)
 
 redu = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, mode='auto')
-
 early = EarlyStopping(monitor='val_loss', min_delta=1e-4, patience=15, verbose=0, mode='auto')
-
 image_path = os.path.join(os.getcwd(),'images')
 gt_path = os.path.join(os.getcwd(), 'ground_truth_mask')
-
 images, labels = getData(image_path, gt_path)
-
 model.fit(images, labels, validation_split=0.2, epochs=10000, batch_size=16, callbacks=[redu,early], verbose=1, shuffle = True)
 model.save('model.h5')
