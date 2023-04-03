84b3d657022cb60b040beb1f0a087cc2971333c4
MarkovChen <98240703+MakovChen@users.noreply.github.com>
2023-04-03 23:53:38 +0800

    Update U-Net.py

--- U-Net.py
+++ U-Net.py
@@ -1,127 +1,103 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
 import tensorflow_datasets as tfds
-import matplotlib.pyplot as plt
+from keras import backend as K
+import tensorflow as tf
 import numpy as np
+import cv2, keras
+import matplotlib.pyplot as plt
 
-#Download Dataset and Preprocessing
-def resize(input_image, input_mask):
-   input_image = tf.image.resize(input_image, (128, 128), method="nearest")
-   input_mask = tf.image.resize(input_mask, (128, 128), method="nearest")
-   return input_image, input_mask
+# Load and Preprocesing to the Oxford-IIIT Pet Dataset from TensorFlow
+dataset = tfds.load('oxford_iiit_pet:3.2.0')
+images, labels = [], []
 
-def augment(input_image, input_mask):
-   if tf.random.uniform(()) > 0.5:
-       # Random flipping of the image and mask
-       input_image = tf.image.flip_left_right(input_image)
-       input_mask = tf.image.flip_left_right(input_mask)
-   return input_image, input_mask
+def resize(input_image, input_label):
+    input_image = cv2.resize(np.array(input_image), (128, 128), cv2.INTER_AREA)
+    input_label = cv2.resize(np.array(input_label), (128, 128), cv2.INTER_AREA)
+    return input_image, input_label 
 
-def normalize(input_image, input_mask):
-   input_image = tf.cast(input_image, tf.float32) / 255.0
-   input_mask -= 1
-   return input_image, input_mask
+def augment(input_image, input_label):
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.flip_left_right(input_image)
+        input_label = tf.image.flip_left_right(input_label)
+    return input_image, input_label
 
-def load_image_train(datapoint):
-   input_image = datapoint["image"]
-   input_mask = datapoint["segmentation_mask"]
-   input_image, input_mask = resize(input_image, input_mask)
-   input_image, input_mask = augment(input_image, input_mask)
-   input_image, input_mask = normalize(input_image, input_mask)
-   return input_image, input_mask
+def normalize(input_image, input_label):
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+    input_label -= 1
+    return input_image, input_label
 
-def load_image_test(datapoint):
-   input_image = datapoint["image"]
-   input_mask = datapoint["segmentation_mask"]
-   input_image, input_mask = resize(input_image, input_mask)
-   input_image, input_mask = normalize(input_image, input_mask)
-   return input_image, input_mask
+for sample in dataset['train']:
+    image, label = resize(sample['image'], sample['segmentation_mask'])
+    images.append(image); labels.append(label) 
+images, labels = np.array(images), np.array(labels)
+X, y = normalize(images, labels)
+X, y = augment(X, y)
+print('images shape:', X.shape)
+print('labels shape:', y.shape)
 
-def display(display_list):
-   plt.figure(figsize=(15, 15))
-   title = ["Input Image", "True Mask", "Predicted Mask"]
-   for i in range(len(display_list)):
-     plt.subplot(1, len(display_list), i+1)
-     plt.title(title[i])
-     plt.imshow(tf.keras.utils.array_to_img(display_list[i]))
-     plt.axis("off")
-   plt.show()
-  
-BATCH_SIZE = 64
-BUFFER_SIZE = 1000
-
-dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
-TRAIN_LENGTH = info.splits["train"].num_examples
-TEST_LENTH = info.splits["test"].num_examples
-
-train_dataset = dataset["train"].map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
-test_dataset = dataset["test"].map(load_image_test, num_parallel_calls=tf.data.AUTOTUNE)
-train_batches = train_dataset.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
-train_batches = train_batches.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-validation_batches = test_dataset.take(3000).batch(BATCH_SIZE)
-test_batches = test_dataset.skip(3000).take(669).batch(BATCH_SIZE)
-
-sample_batch = next(iter(train_batches))
-random_index = np.random.choice(sample_batch[0].shape[0])
-sample_image, sample_mask = sample_batch[0][random_index], sample_batch[1][random_index]
-display([sample_image, sample_mask])
-
-#Build Unet
+'''
+[Build the Unet model]
+'''
 def double_conv_block(x, n_filters):
-   x = layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
-   x = layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
-   return x
+    x = tf.keras.layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
+    x = tf.keras.layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
+    return x
 
 def downsample_block(x, n_filters):
-   f = double_conv_block(x, n_filters)
-   p = layers.MaxPool2D(2)(f)
-   p = layers.Dropout(0.3)(p)
-   return f, p
+    f = double_conv_block(x, n_filters)
+    p = tf.keras.layers.MaxPool2D(2)(f)
+    p = tf.keras.layers.Dropout(0.3)(p)
+    return f, p
 
 def upsample_block(x, conv_features, n_filters):
-   x = layers.Conv2DTranspose(n_filters, 3, 2, padding="same")(x)
-   x = layers.concatenate([x, conv_features])
-   x = layers.Dropout(0.3)(x)
-   x = double_conv_block(x, n_filters)
-   return x
+    x = tf.keras.layers.Conv2DTranspose(n_filters, 3, 2, padding="same")(x)
+    x = tf.keras.layers.concatenate([x, conv_features])
+    x = tf.keras.layers.Dropout(0.3)(x)
+    x = double_conv_block(x, n_filters)
+    return x
 
-def Unet():
-   inputs = layers.Input(shape=(128,128,3))
+def UNet(inputs):
+    # encoder: contracting path - downsample
+    f1, p1 = downsample_block(inputs, 64)
+    f2, p2 = downsample_block(p1, 128)
+    f3, p3 = downsample_block(p2, 256)
+    f4, p4 = downsample_block(p3, 512)
 
-   f1, p1 = downsample_block(inputs, 64)
-   f2, p2 = downsample_block(p1, 128)
-   f3, p3 = downsample_block(p2, 256)
-   f4, p4 = downsample_block(p3, 512)
-   bottleneck = double_conv_block(p4, 1024)
+    # 5 - bottleneck
+    bottleneck = double_conv_block(p4, 1024)
 
-   u6 = upsample_block(bottleneck, f4, 512)
-   u7 = upsample_block(u6, f3, 256)
-   u8 = upsample_block(u7, f2, 128)
-   u9 = upsample_block(u8, f1, 64)
+    # decoder: expanding path - upsample
+    u6 = upsample_block(bottleneck, f4, 512)
+    u7 = upsample_block(u6, f3, 256)
+    u8 = upsample_block(u7, f2, 128)
+    u9 = upsample_block(u8, f1, 64)
 
-   outputs = layers.Conv2D(3, 1, padding="same", activation = "softmax")(u9)
-   unet_model = tf.keras.Model(inputs, outputs, name="U-Net")
-   return unet_model
-  
-unet_model = Unet()
-unet_model.compile(optimizer=tf.keras.optimizers.Adam(), loss="sparse_categorical_crossentropy", metrics="accuracy")
-model_history = unet_model.fit(train_batches, epochs=20, steps_per_epoch=TRAIN_LENGTH // BATCH_SIZE, validation_steps=TEST_LENTH // BATCH_SIZE // 5, validation_data=test_batches)
+    outputs = tf.keras.layers.Conv2D(3, 1, padding="same", activation = "softmax")(u9)
+    model = tf.keras.Model(inputs, outputs, name="U-Net")
+    return model
+    
+def segmentation_model():
+    num_classes = len(np.unique(y)) #0, 1, 2
+    inputs_shape = (128, 128, 3)
+    inputs = tf.keras.layers.Input(shape = inputs_shape)
+    model = UNet(inputs)
+    model.summary()
+    return model
 
-#Demo
-def create_mask(pred_mask):
- pred_mask = tf.argmax(pred_mask, axis=-1)
- pred_mask = pred_mask[..., tf.newaxis]
- return pred_mask[0]
-def show_predictions(dataset=None, num=1):
- if dataset:
-   for image, mask in dataset.take(num):
-     pred_mask = unet_model.predict(image)
-     display([image[0], mask[0], create_mask(pred_mask)])
- else:
-   display([sample_image, sample_mask,
-            create_mask(model.predict(sample_image[tf.newaxis, ...]))])
-count = 0
-for i in test_batches:
-   count +=1
-print("number of batches:", count)
+model = segmentation_model()
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+model.fit(X[:-1], y[:-1], batch_size=32, epochs=10)
+
+'''
+[Utilize AND Demo]
+'''
+def display(display_list):
+  plt.figure(figsize=(15, 15))
+  title = ["Input Image", "True Mask", "Predicted Mask"]
+  for i in range(len(display_list)):
+    plt.subplot(1, len(display_list), i+1)
+    plt.title(title[i])
+    plt.imshow(tf.keras.utils.array_to_img(display_list[i]))
+    plt.axis("off")
+    plt.show()
+pred = model.predict(X[-1:])
+display([X[-1], np.array(y[-1]).reshape(128, 128, 1), np.argmax(pred[-1], axis=2).reshape(128, 128, 1)])
